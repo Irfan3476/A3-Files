8320e5d01604c8628b3c022b6e301516bf478456
Irfan3476 <147170719+Irfan3476@users.noreply.github.com>
2023-12-01 14:38:03 +0000

    Update RIP.py

--- RIP.py
+++ RIP.py
@@ -1,6 +1,6 @@
-#Import required modules/packages/library
+# Import required modules/packages/library
 import pexpect
-#Define Variables
+# Define Variables
 ip_address = '192.168.56.101'
 username = 'prne'
 password = 'cisco123!'
@@ -12,7 +12,7 @@ session = pexpect.spawn('ssh ' + username + '@' + ip_address, encoding='utf-8', 
 
 result = session.expect(['Password:', pexpect.TIMEOUT, pexpect.EOF])
 
-#Check for error, if an error occurs display it and then exit
+# Check for error, if an error occurs display it and then exit
 if result != 0:
     print('--- FAILURE! creating a session for: ', ip_address)
     exit()
@@ -24,25 +24,25 @@ if result != 0:
     print('--- FAILURE! entering password: ', password)
     exit()
 
-#Enter enable mode
+# Enter enable mode
 session.sendline('enable')
 result = session.expect(['Password:', pexpect.TIMEOUT, pexpect.EOF])
 
-#Check for error, if an error occurs display it and then exit
+# Check for error, if an error occurs display it and then exit
 if result != 0:
     print('--- Failure! entering enable mode')
     exit()
 
-#Send enable password details
+# Send enable password details
 session.sendline(password_enable)
 result = session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
 
-#Check for error, if an error occurs display it and then exit
+# Check for error, if an error occurs display it and then exit
 if result != 0:
     print('--- Failure! entering enable mode after sending password')
     exit()
 
-#Enter configuration mode
+# Enter configuration mode
 session.sendline('configure terminal')
 
 result = session.expect([r'.\(config\)#', pexpect.TIMEOUT, pexpect.EOF])
@@ -55,18 +55,22 @@ result = session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
 session.sendline('ip add 192.168.61.103 255.255.255.0')
 result = session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
 
+# Ensuring that the interface is up 
 session.sendline('no shutdown')
 result = session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
 
 # Exiting the GigabitEthernet 2 interface
 session.sendline('exit')
 
+# Enables a RIP routing proccess
 session.sendline('router rip')
 result = session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
 
+# Associating the appropriate address to the RIP routing process in this case being the ip address within the range of GigabitEthernet1
 session.sendline('network 192.168.56.0')
 result = session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
 
+# Associating the appropriate address to the RIP routing process in this case being the ip address within the range of GigabitEthernet2
 session.sendline('network 192.168.61.0')
 result = session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
 
@@ -83,8 +87,8 @@ print('---               Username: ', username)
 print('---               Password: ', password)
 print('') 
 print('------------------------------------------')
-print('Configured looback address')
 print('Changed ip address of Gigabit Ethernet 2')
+print('configured RIP')
 
 
 
